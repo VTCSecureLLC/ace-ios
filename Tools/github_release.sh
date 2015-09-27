34bf16d8a40acf95badcf7fae5db7ca84c20c5f1
Ian Blenke <ian@blenke.com>
2015-09-26 21:44:44 -0400

    Adding comments, securing credentials

--- Tools/github_release.sh
+++ Tools/github_release.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Only deploy master branch builds
+
 if [ -z "$TRAVIS_BRANCH" ] ; then
   echo "TRAVIS_BRANCH not found. Deploy skipped"
   exit 0
@@ -12,6 +14,8 @@ fi
 
 set -xe
 
+# Generate an archive for this project
+
 XCARCHIVE_FILE=/tmp/ace-ios.xcarchive
 
 xctool -project linphone.xcodeproj \
@@ -22,6 +26,8 @@ xctool -project linphone.xcodeproj \
        archive \
        -archivePath $XCARCHIVE_FILE 1>/dev/null
 
+# Prepare semantic versioning tag
+
 SHA1=$(git rev-parse --short HEAD)
 
 echo "$(bundle exec semver)-${TRAVIS_BUILD_NUMBER:-1}"-${SHA1} > LastCommit.txt
@@ -29,9 +35,15 @@ git log -1 --pretty=format:%B >> LastCommit.txt
 
 tag="$(bundle exec semver)-${TRAVIS_BUILD_NUMBER:-1}"-${SHA1}
 
+# Prepare other variables
+
 IFS=/ GITHUB_REPO=($TRAVIS_REPO_SLUG)
 
+# Create a GitHub release if credentials are available
+
+set +x
 if [ -n "$GITHUB_TOKEN" ]; then
+  set -x
 
   curl -sL https://github.com/aktau/github-release/releases/download/v0.6.2/darwin-amd64-github-release.tar.bz2 | \
     bunzip2 -cd | \
@@ -49,8 +61,14 @@ if [ -n "$GITHUB_TOKEN" ]; then
 
 fi
 
+# Release via HockeyApp if credentials are available
+
+set +x
 if [ -n "$HOCKEYAPP_TOKEN" ]; then
+  set -x
   IPA_FILE=/tmp/ace-ios.ipa
+
+  # Generate an ipa from the archive
 
   xcodebuild -exportArchive \
              -exportFormat ipa \
@@ -58,9 +76,13 @@ if [ -n "$HOCKEYAPP_TOKEN" ]; then
              -exportPath $IPA_FILE \
              -exportProvisioningProfile 'iOSTeam Provisioning Profile: com.vtcsecure.*'
 
-  DSYM_DIR=$(find . -name '*.dSYM' | head -1)
-  DSYM_ZIP_FILE=/tmp/APP.dsym.zip
+  # Create a dSYM zip file from the archive build
+
+  DSYM_DIR=$(find build/derived -name '*.dSYM' | head -1)
+  DSYM_ZIP_FILE=${IPA_FILE}.dsym.zip
   (cd $(dirname $DSYM_DIR) ; zip -r $DSYM_ZIP_FILE $(basename $DSYM_DIR) )
+
+  # Distribute via HockeyApp
 
   bundle exec ipa distribute:hockeyapp \
              --token $HOCKEYAPP_TOKEN \
