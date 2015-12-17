f40b05f870efd7ebd662d4247408bd8355e35aa2
Zack Matthews <zack@zackmatthews.com>
2015-12-16 19:03:55 -0500

    VATRP-1453: Populate deaf/HoH resources from cdn.vatrp.net

---
+++ Classes/ResourcesViewController.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+#import "UIToggleButton.h"
+#import "UICompositeViewController.h"
+@interface ResourcesViewController : UIViewController<UITextFieldDelegate, UITableViewDataSource,UITableViewDelegate> {}
+
+@end
