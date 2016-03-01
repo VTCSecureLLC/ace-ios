1dcd678b5f522f08d6c0a52ba58a2ae286b6762e
Hrachya Stepanyan <hrachya.stepanyan.84@gmail.com>
2016-03-01 19:29:20 +0400

    Added IncallButton

---
+++ Classes/IncallButton.m
@@ -0,0 +1,21 @@
+//
+//  IncallButton.m
+//  linphone
+//
+//  Created by Hrachya Stepanyan on 3/1/16.
+//
+//
+
+#import "IncallButton.h"
+
+@implementation IncallButton
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
