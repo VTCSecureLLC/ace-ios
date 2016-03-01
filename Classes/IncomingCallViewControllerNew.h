a7e62c742dbc7b28173e18e1025084be583fb314
Hrachya Stepanyan <hrachya.stepanyan.84@gmail.com>
2016-03-01 17:12:57 +0400

    Added IncomingCallViewController

---
+++ Classes/IncomingCallViewControllerNew.h
@@ -0,0 +1,13 @@
+//
+//  IncomingCallViewControllerNew.h
+//  linphone
+//
+//  Created by Hrachya Stepanyan on 3/1/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IncomingCallViewControllerNew : UIViewController
+
+@end
