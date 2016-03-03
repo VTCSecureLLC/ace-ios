46251171d5e4358b790d4a1600ad9bae51cffd03
Ian Blenke <ian@blenke.com>
2016-03-03 15:49:11 -0500

    Revert "Merge pull request #306 from shareefalis/linphonecre"
    This reverts commit 4261b59ed7f9acabcdad8ad62912216df97b0009, reversing
    changes made to bc1167a99123d159ceecde92b7ee2726fca05a03.

--- prepare.py
+++ prepare.py
@@ -52,6 +52,7 @@ class IOSTarget(prepare.Target):
         self.output = 'liblinphone-sdk/' + arch + '-apple-darwin.ios'
         self.additional_args = [
             '-DCMAKE_INSTALL_MESSAGE=LAZY',
+            '-DLINPHONE_BUILDER_GROUP_EXTERNAL_SOURCE_PATH_BUILDERS=YES',
             '-DLINPHONE_BUILDER_EXTERNAL_SOURCE_PATH=' +
             current_path + '/submodules'
         ]
@@ -517,7 +518,7 @@ def main(argv=None):
                 error("Could not clone tunnel. Please see http://www.belledonne-communications.com/voiptunnel.html")
                 return 1
         warning("Tunnel enabled, disabling GPL third parties.")
-        additional_args += ["-DENABLE_TUNNEL=ON", "-DENABLE_GPL_THIRD_PARTIES=OFF", "-DENABLE_FFMPEG=NO"]
+        additional_args += ["-DENABLE_TUNNEL=ON", "-DENABLE_GPL_THIRD_PARTIES=OFF"]
 
     # User's options are priority upon all automatic options
     additional_args += additional_args2
