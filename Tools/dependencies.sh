236bbe4be60c298645b15ae6752249244f084a6e
Shareef Ali <shareefalis@gmail.com>
2016-04-08 11:45:43 -0400

    paralelllize submodule  init

--- Tools/dependencies.sh
+++ Tools/dependencies.sh
@@ -7,10 +7,11 @@ which brew || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/H
 
 #pushd /usr/local/Library/Homebrew/; git branch --set-upstream-to=origin/master master ; git pull ; popd
 brew update 1>/dev/null
-brew install doxygen nasm yasm intltool ninja antlr cmake
+brew install doxygen nasm yasm optipng imagemagick intltool ninja antlr cmake
 wget --no-check-certificate https://raw.githubusercontent.com/FFmpeg/gas-preprocessor/master/gas-preprocessor.pl
 chmod +x gas-preprocessor.pl
 sudo mv -f gas-preprocessor.pl /usr/local/bin
 [ -x /usr/local/bin/libtoolize ] || sudo ln -sf /usr/local/bin/glibtoolize /usr/local/bin/libtoolize
-git submodule update --init --recursive
+cat .gitmodules | grep -o '".*"' | cut -d '"' -f 2 | while sleep 0.1 && read line; do git submodule update --init --recursive $line & done
+
 #bundle install
