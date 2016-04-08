f9a6d88f5912eb4da243781ebd3721f0b5bdd85d
Shareef Ali <shareefalis@gmail.com>
2016-04-08 16:19:12 -0400

    fixasync

--- Tools/dependencies.sh
+++ Tools/dependencies.sh
@@ -6,14 +6,18 @@ cd $DIR/..
 which brew || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 
-brew update 1>/dev/null && brew install doxygen nasm yasm optipng imagemagick intltool ninja antlr cmake &
-cat .gitmodules | grep -o '".*"' | cut -d '"' -f 2 | while sleep 1 && read line; do git submodule update --init --recursive $line & done
+cat .gitmodules | grep -o '".*"' | cut -d '"' -f 2 | while sleep .1 && read line; do git submodule update --init --recursive $line & done
+wget --no-check-certificate https://raw.githubusercontent.com/FFmpeg/gas-preprocessor/master/gas-preprocessor.pl &
+chmod +x gas-preprocessor.pl &
+sudo mv -f gas-preprocessor.pl /usr/local/bin &
+[ -x /usr/local/bin/libtoolize ] || sudo ln -sf /usr/local/bin/glibtoolize /usr/local/bin/libtoolize &
+bundle install &
 
+
+brew update 1>/dev/null && brew install doxygen nasm yasm optipng imagemagick intltool ninja antlr cmake
 
 #pushd /usr/local/Library/Homebrew/; git branch --set-upstream-to=origin/master master ; git pull ; popd
-wget --no-check-certificate https://raw.githubusercontent.com/FFmpeg/gas-preprocessor/master/gas-preprocessor.pl
-chmod +x gas-preprocessor.pl
-sudo mv -f gas-preprocessor.pl /usr/local/bin
-[ -x /usr/local/bin/libtoolize ] || sudo ln -sf /usr/local/bin/glibtoolize /usr/local/bin/libtoolize
 
-bundle install
+
+# make sure everything is fetched
+git submodule update --init --recursive
