3448ed653bde850fcd36690ba41171720859cbb0
Shareef Ali <shareefalis@gmail.com>
2016-04-08 16:31:43 -0400

    on top of my head improvement

--- Tools/dependencies.sh
+++ Tools/dependencies.sh
@@ -6,16 +6,18 @@ cd $DIR/..
 which brew || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 
-cat .gitmodules | grep -o '".*"' | cut -d '"' -f 2 | while sleep 2 && read line; do git submodule update --init --recursive $line & done
+
 wget --no-check-certificate https://raw.githubusercontent.com/FFmpeg/gas-preprocessor/master/gas-preprocessor.pl &
 chmod +x gas-preprocessor.pl &
 sudo mv -f gas-preprocessor.pl /usr/local/bin &
 [ -x /usr/local/bin/libtoolize ] || sudo ln -sf /usr/local/bin/glibtoolize /usr/local/bin/libtoolize &
 bundle install &
 
+cat .gitmodules | grep -o '".*"' | cut -d '"' -f 2 | while sleep 2 && read line; do git submodule update --init --recursive $line & done
 # make sure everything is fetched
+
 git submodule update --init --recursive &
-brew update 1>/dev/null && brew install doxygen nasm yasm optipng imagemagick intltool ninja antlr cmake 
+brew update 1>/dev/null && brew install doxygen nasm yasm optipng imagemagick intltool ninja antlr cmake
 
 #pushd /usr/local/Library/Homebrew/; git branch --set-upstream-to=origin/master master ; git pull ; popd
 
