053de7bbe2c0a5de10a73885bd0d86ae9902705b
Shareef Ali <shareefalis@gmail.com>
2016-04-08 16:00:40 -0400

    async more things

--- Tools/dependencies.sh
+++ Tools/dependencies.sh
@@ -3,14 +3,14 @@ set -xe
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/..
 
-cat .gitmodules | grep -o '".*"' | cut -d '"' -f 2 | while sleep .4 && read line; do git submodule update --init --recursive $line & done
-
-
 which brew || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+
+cat .gitmodules | grep -o '".*"' | cut -d '"' -f 2 | while sleep .4 && read line; do git submodule update --init --recursive $line & done
+
+brew update 1>/dev/null && brew install doxygen nasm yasm optipng imagemagick intltool ninja antlr cmake &
+
 #pushd /usr/local/Library/Homebrew/; git branch --set-upstream-to=origin/master master ; git pull ; popd
-brew update 1>/dev/null
-brew install doxygen nasm yasm optipng imagemagick intltool ninja antlr cmake
 wget --no-check-certificate https://raw.githubusercontent.com/FFmpeg/gas-preprocessor/master/gas-preprocessor.pl
 chmod +x gas-preprocessor.pl
 sudo mv -f gas-preprocessor.pl /usr/local/bin
