71c37c845dd091c8ff51bb64751d0dbc6ac15a8a
Ian Blenke <ian@blenke.com>
2016-04-11 18:12:54 -0400

    Fixed variable expansion

--- Tools/travis_compile.sh
+++ Tools/travis_compile.sh
@@ -7,17 +7,16 @@ mkdir -p sdkcache/
 git submodule | cut -d'(' -f1 | sort > sdkcache/current.txt
 which md5sum || brew install md5sha1sum
 SUBMODULE_HASH=$(md5sum sdkcache/current.txt | awk '{print $1}')
-mv sdkcache/current.txt liblinphone-sdk_$(SUBMODULE_HASH)_submodules.txt
+mv sdkcache/current.txt liblinphone-sdk_${SUBMODULE_HASH}_submodules.txt
 which aws || brew install awscli
 BUCKET=${BUCKET:-vtcsecurellc-travis}
 CACHE=${BUCKET}-cache
-aws iam get-user
 if ! aws s3 sync s3://$CACHE/ace-ios/sdkcache/ sdkcache/ ; then
   echo "Could not sync s3://$CACHE/ace-ios/sdkcache/ to sdkcache/
 fi
 
-if [ -f sdkcache/liblinphone-sdk_$(SUBMODULE_HASH).zip ] ; then
-  unzip sdkcache/liblinphone-sdk_$(SUBMODULE_HASH).zip
+if [ -f sdkcache/liblinphone-sdk_${SUBMODULE_HASH}.zip ] ; then
+  unzip sdkcache/liblinphone-sdk_${SUBMODULE_HASH}.zip
   exit 0
 else
   echo "There is no existing cached SDK build for this set of submodules."
@@ -48,7 +47,7 @@ echo "Running make for dependencies"
 if $DIR/compile.sh >> $LOGFILE 2>&1
 then
   MAKE_RESULT=$?
-  zip -r sdkcache/liblinphone-sdk_$(SUBMODULE_HASH).zip liblinphone-sdk
+  zip -r sdkcache/liblinphone-sdk_${SUBMODULE_HASH}.zip liblinphone-sdk
   if ! aws s3 sync sdkcache/ s3://$CACHE/ace-ios/sdkcache/ ; then
     echo "Error $?: Encountered a problem syncing sdkcache/ folder to s3://$CACHE/ace-ios/sdkcache/"
   fi
