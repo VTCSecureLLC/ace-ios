9626fdc58deb3786434ac1a888af60d40d0e36b0
Shareef Ali <shareef.ali@vtcsecure.com>
2016-04-19 22:37:59 -0400

    Clean up build script

--- Tools/travis_depends.sh
+++ Tools/travis_depends.sh
@@ -3,22 +3,17 @@ set -xe
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/..
 git submodule update --init Classes/KIF &
+bundle install &
 mkdir -p sdkcache/
 git submodule | cut -d'(' -f1 | awk '{print substr($0, 2, length($0))}'  | sort > current.txt
 which md5sum || brew install md5sha1sum
 SUBMODULE_HASH=$(md5sum current.txt | awk '{print $1}')
-cp current.txt liblinphone-sdk_${SUBMODULE_HASH}_submodules.txt
-bundle install &
 which aws || brew install awscli
 BUCKET=${BUCKET:-vtcsecurellc-travis}
 CACHE=${BUCKET}-cache
 
 if ! aws s3 cp s3://$CACHE/ace-ios/sdkcache/liblinphone-sdk_${SUBMODULE_HASH}.zip sdkcache/liblinphone-sdk_${SUBMODULE_HASH}.zip ; then
   echo "Could not sync s3://$CACHE/ace-ios/sdkcache/liblinphone-sdk_${SUBMODULE_HASH}.zip to sdkcache/liblinphone-sdk_${SUBMODULE_HASH}.zip"
-fi
-
-if ! aws s3 sync s3://$CACHE/ace-ios/sdkcache/liblinphone-sdk_${SUBMODULE_HASH}_submodules.txt sdkcache/liblinphone-sdk_${SUBMODULE_HASH}_submodules.txt ; then
-  echo "Could not sync s3://$CACHE/ace-ios/sdkcache/liblinphone-sdk_${SUBMODULE_HASH}_submodules.txt to sdkcache/liblinphone-sdk_${SUBMODULE_HASH}_submodules.txt"
 fi
 
 if [ -f sdkcache/liblinphone-sdk_${SUBMODULE_HASH}.zip ] ; then
