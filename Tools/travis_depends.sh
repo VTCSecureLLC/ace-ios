8af1ea911024007288d1b4f5244ef3418ebf8a3d
Shareef Ali <shareefalis@gmail.com>
2016-04-20 15:03:01 -0400

    testing1

--- Tools/travis_depends.sh
+++ Tools/travis_depends.sh
@@ -15,8 +15,8 @@ mkdir -p sdkcache/
 git submodule | cut -d'(' -f1 | awk '{print substr($0, 2, length($0))}'  | sort > current.txt
 brew install md5sha1sum awscli
 SUBMODULE_HASH=$(md5sum current.txt | awk '{print $1}')
-BUCKET=${BUCKET:-vtcsecurellc-travis}
-CACHE=${BUCKET}-cache
+#BUCKET=${BUCKET:-vtcsecurellc-travis}
+#CACHE=${BUCKET}-cache
 
 if ! aws s3 cp s3://$CACHE/ace-ios/sdkcache/liblinphone-sdk_${SUBMODULE_HASH}.zip sdkcache/liblinphone-sdk_${SUBMODULE_HASH}.zip ; then
   echo "Could not sync s3://$CACHE/ace-ios/sdkcache/liblinphone-sdk_${SUBMODULE_HASH}.zip to sdkcache/liblinphone-sdk_${SUBMODULE_HASH}.zip"
