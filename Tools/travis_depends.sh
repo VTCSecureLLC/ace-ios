e5ae1268137afd99a6a4aa9a82c2328ba66cdc9f
Shareef Ali <shareefalis@gmail.com>
2016-04-20 14:58:13 -0400

    handle zombies better

--- Tools/travis_depends.sh
+++ Tools/travis_depends.sh
@@ -4,10 +4,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/..
 
 function spawn_fork {
-local run_this=$1 wait_for=${2:-1000}
-bash -c "$run_this" &
-local dat_pid=$!
-( sleep $wait_for ; if kill -0 $dat_pid ; then kill $dat_pid ; fi ) &
+local run_this=$1
+$run_this &
 }
 
 spawn_fork "git submodule update --init Classes/KIF"
@@ -15,9 +13,8 @@ spawn_fork "bundle install"
 
 mkdir -p sdkcache/
 git submodule | cut -d'(' -f1 | awk '{print substr($0, 2, length($0))}'  | sort > current.txt
-which md5sum || brew install md5sha1sum
+brew install md5sha1sum awscli
 SUBMODULE_HASH=$(md5sum current.txt | awk '{print $1}')
-which aws || brew install awscli
 BUCKET=${BUCKET:-vtcsecurellc-travis}
 CACHE=${BUCKET}-cache
 
@@ -32,3 +29,5 @@ else
  Tools/dependencies.sh
 fi
 wait
+
+( sleep 1000 ; ps -eo state,ppid | awk '$1=="Z"{cmd="kill -9 "$2;system(cmd) }') &
