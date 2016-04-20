e1c11abb8b7eb583cf67c600c5ab0f8b78f4b140
Shareef Ali <shareef.ali@vtcsecure.com>
2016-04-20 00:01:18 -0400

    Update dependencies.sh

--- Tools/dependencies.sh
+++ Tools/dependencies.sh
@@ -3,17 +3,9 @@ set -xe
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/..
 
-
 wget --no-check-certificate https://raw.githubusercontent.com/FFmpeg/gas-preprocessor/master/gas-preprocessor.pl && chmod +x gas-preprocessor.pl && sudo mv -f gas-preprocessor.pl /usr/local/bin &
 bundle install &
-
-
-# make sure everything is fetched
-
-
 ( which brew || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" ) && brew update 1>/dev/null && brew install doxygen nasm yasm optipng imagemagick intltool ninja antlr cmake &&  ([ -x /usr/local/bin/libtoolize ] || sudo ln -sf /usr/local/bin/glibtoolize /usr/local/bin/libtoolize ) &
-git submodule update --init --recursive
+git submodule update --init --recursive &
 
-#pushd /usr/local/Library/Homebrew/; git branch --set-upstream-to=origin/master master ; git pull ; popd
-
-
+wait
