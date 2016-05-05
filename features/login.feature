c18ef3241bc57b887c272909fda8a82d23d2b7ba
codaengineer <codaengineer@gmail.com>
2016-05-05 08:16:36 -0600

    Updated some step definitions

--- features/login.feature
+++ features/login.feature
@@ -3,7 +3,17 @@ Feature: Logging into the app
 	As an ACE user
 	I want to log into the app
 
-@login_scenario_1
+@login_successful
 Scenario: Successful login
 	Given I am on the Welcome Screen
-	When I touch 
+	When I touch 'Select Provider' button
+	Then I see a dropdown list
+	Then I select 'STL Test'
+	Then I touch 'Select' button
+	Then I clear input field 1
+	Then I clear input field 2
+	And I turn "off" auto correct
+	And I fill 'username' field
+	And I fill 'password' field
+	Then I touch 'Login' button
+ 
--- features/step_definitions/firstrun.rb
+++ features/step_definitions/firstrun.rb
@@ -2,31 +2,7 @@ Given(/^I am on the ToS agreement screen$/) do
 	check_element_exists("UIButton text:'Accept'")
 end
 
-#When(/^I see the '"ACE" Would Like to Access Your Contacts' alert$/) do
-#	check_element_exists("label text:'\"ACE\" Would Like to Access Your Contacts'")
-#end
-
-#Then(/^I touch the 'OK' button$/) do
-#	touch("view marked:'OK'") 
-#end
-
-#Then(/^I see the '"ACE" Would Like to Send You Notifications' alert$/) do
-#  check_element_exists("label text:'\"ACE\" Would Like to Send You Notifications'") 
-#end
-
-#Then(/^I touch the 'OK' button$/) do
-#	touch("view marked:'OK'") 
-#end
-
-#Then(/^I see the 'Allow "ACE" to access your location while you use the app\?' alert$/) do
-#	check_element_exists("label text:'Allow \"ACE\" to access your location while you use the app\?'")
-#end
-
-#Then(/^I touch the 'Allow' button$/) do
-#	touch("view marked:'Allow'")
-#end
-
 Then(/^I touch the 'Accept' button$/) do
-	touch("UIButton index:9")
+	touch("button index:0")
 end
 
